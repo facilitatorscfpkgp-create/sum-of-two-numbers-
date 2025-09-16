9f16daad49d6ba0396b935a16583568423979611
facilitatorscfpkgp-create <facilitatorscfpkgp@gmail.com>
2025-09-16 00:26:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let SUM = 0
+let NUMBER_1 = 58
+let NUMBER2 = 45
+if (NUMBER_1 > NUMBER2) {
+    SUM = NUMBER_1 + NUMBER2
+} else {
+    SUM = NUMBER2 + NUMBER_1
+}
+game.splash("SUM OF ALL NUMBERS ", SUM)
